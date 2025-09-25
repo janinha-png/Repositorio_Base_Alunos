6b2ce13a7bbb2bbc71b7c65ed45f350572dceda1
janinha <jjjoana726@gmail.com>
2025-09-25 11:50:18 -0300

    aula de api pagina web
    filmes html

--- Modulo 2/APIs/cidade de salem.py
+++ Modulo 2/APIs/cidade de salem.py
@@ -1,10 +1,10 @@
 import requests
 usuario = {
-    "name":"Lazaro barbosa",
+    "name":"Joana",
 }
 #response = requests.post('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos',json=usuario)
 
-response = requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/4')
+response = requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/')
 
 print(response.status_code)
 print(response.json())
